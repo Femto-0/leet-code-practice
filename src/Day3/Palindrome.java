58d2d5b23a1e88e33a604c3eea006cb9e8d892d1
Ritesh Shah <arits.king74@gmail.com>
2023-10-15 02:06:41 -0500

    added more conditions to the class to cover all possible cases and decrese memory usage

--- src/Day3/Palindrome.java
+++ src/Day3/Palindrome.java
@@ -12,34 +12,37 @@ public class Palindrome {
 //        if (arr[0] != arr[1]) {
 //            return false;
 //        }
+if(x>=10) {
+    String convert = String.valueOf(x);
+    int[] arr = new int[convert.length()];
+    for (int a = 0; a <= convert.length() - 1; a++) {
+        arr[a] = convert.charAt(a);
+    }
+    int[] arr1 = new int[convert.length() / 2];
+    int[] arr2 = new int[convert.length() / 2];
+    int num = arr.length / 2;
+    for (int i = 0; i <= num - 1; i++) {
+        arr1[i] = arr[i];
+    }
+    int y = 0;
+    if (arr.length % 2 != 0) {
+        num += 1;
+    }
+    for (int j = arr.length - 1; j >= num; j--) {
+        arr2[y] = arr[j];
+        y++;
+    }
 
-        String convert=String.valueOf(x);
-        if(convert.length()!=1) {
-            int[] arr = new int[convert.length()];
-            for (int a = 0; a <= convert.length() - 1; a++) {
-                arr[a] = convert.charAt(a);
-            }
-            int[] arr1 = new int[convert.length() / 2];
-            int[] arr2 = new int[convert.length() / 2];
-            int num = arr.length / 2;
-            for (int i = 0; i <= num - 1; i++) {
-                arr1[i] = arr[i];
-            }
-            int y = 0;
-            if (arr.length%2!=0) {
-                num += 1;
-            }
-            for (int j = arr.length - 1; j >= num; j--) {
-                arr2[y] = arr[j];
-                y++;
-            }
-
-            for (int i = 0; i <= arr1.length - 1; i++) {
-                if (arr1[i] != arr2[i]) {
-                    return false;
-                }
-            }
+    for (int i = 0; i <= arr1.length - 1; i++) {
+        if (arr1[i] != arr2[i]) {
+            return false;
         }
+    }
+}else if(x<0){
+    return false;
+}else {
+    return true;
+}
     return  true;
     }
 
