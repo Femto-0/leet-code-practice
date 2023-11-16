c8ec7c6d3b71f865b5b725a604532c986d5c2ff9
Ritesh Shah <arits.king74@gmail.com>
2023-11-16 00:01:06 -0600

    corrected the algorithm, covers all the possible test cases

--- src/LongestCommonPrefix.java
+++ src/LongestCommonPrefix.java
@@ -3,14 +3,12 @@ import java.util.Objects;
 public class LongestCommonPrefix {
 
     public String longestCommonPrefix(String[] strs) {
-        boolean checker = true;
-        int[] arrRecord = new int[strs.length - 1];
+        int[] arrRecord = new int[strs.length-1];
         int count = 0;
-        while (checker) {
             if (strs.length <= 1) {
                 return strs[0];
 
-            } else {
+           }else {
                 for (int i = 0; i <= strs.length - 1; i++) {
                     if (strs[i].isEmpty()) {
                         return "";
@@ -20,6 +18,7 @@ public class LongestCommonPrefix {
             String first;
             String second;
             for (int i = 0; i <= strs.length - 2; i++) {
+                count=0;
                 first = strs[i];
                 second = strs[i + 1];
                 if (!Objects.equals(first.charAt(0), second.charAt(0))) {
@@ -27,23 +26,21 @@ public class LongestCommonPrefix {
                 } else {
                     for (int j = 0; j <= Math.min(first.length(), second.length()) - 1; j++) {
                         if (Objects.equals(first.charAt(j), second.charAt(j))) {
-                            count = j + 1;
-                        } else {
+                            count++;
+                        }else {
                             arrRecord[i] = count;
-                            count=0;
                             break;
                         }
                     }
+                    arrRecord[i]=count; //in case all the characters match, and it never enters the "else" loop.
                 }
             }
 
-            checker = false;
-        }
-
         StringBuilder sb = new StringBuilder();
-        for (int i = 0; i < arrRecord.length - 1; i++) {
-            if (count < arrRecord[i]) {
+        for (int i = 0; i <= arrRecord.length - 1; i++) {
+            if (count > arrRecord[i] && arrRecord[i]!=0) {
                 count = arrRecord[i];
+
             }
         }
         for (int i = 0; i <= count - 1; i++) {
